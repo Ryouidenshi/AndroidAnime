01e88f40ba9fc28f6d964ea01d13a92ebbef84b6
Ryouidenshi <djonich@mail.ru>
2022-06-27 16:46:48 +0500

    final

--- app/src/main/java/com/example/android_final/model/AnimeList.kt
+++
@@ -1,8 +0,0 @@
-package com.example.android_final.model
-
-import com.example.android_final.model.Anime
-import com.google.gson.annotations.SerializedName
-
-data class AnimeList(
-    @SerializedName("results") val animeList: List<Anime>
-)
